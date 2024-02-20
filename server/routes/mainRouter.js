84749775c63305e64985e8bd40c30d1326afef76
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-20 17:44:02 +0100

    Tricky way to insert data to Mongo Atlas (creating a tempo function that I'll delete now)

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import Post from '../models/Post.js'
 
 const router = express.Router()
 
@@ -11,6 +12,55 @@ router.get('/', (_req, res) => {
 
   res.render('index', locals)
 })
+
+function insertPostData() {
+
+  Post.insertMany([
+    {
+      title: '10 Essential Tips for Beginner Bloggers',
+      body: 'Learn valuable insights on how to kickstart your blogging journey with these expert tips.'
+    },
+    {
+      title: 'The Power of Visual Content in Blogging',
+      body: 'Discover why incorporating captivating visuals can significantly enhance the impact of your blog posts.'
+    },
+    {
+      title: 'Mastering the Art of SEO: Boost Your Blogs Visibility',
+      body: 'Explore advanced strategies and techniques to optimize your blog for search engines and attract more organic traffic.'
+    },
+    {
+      title: 'Unleashing Creativity: Finding Your Unique Blogging Voice',
+      body: 'Unlock your creative potential and learn how to develop a distinctive voice that resonates with your audience.'
+    },
+    {
+      title: 'The Evolution of Blogging: Trends to Watch in 2024',
+      body: 'Stay ahead of the curve by exploring the latest trends shaping the blogging landscape in the digital age.'
+    },
+    {
+      title: 'From Passion to Profit: Monetization Strategies for Bloggers',
+      body: 'Turn your passion for blogging into a lucrative venture with effective monetization strategies and revenue streams.'
+    },
+    {
+      title: 'Navigating the Challenges of Blogging: Overcoming Writers Block',
+      body: 'Discover practical tips and techniques to overcome writers block and maintain consistent productivity in your blogging journey.'
+    },
+    {
+      title: 'The Importance of Community: Building Connections Through Blogging',
+      body: 'Explore the benefits of fostering a supportive community around your blog and cultivating meaningful relationships with your audience.'
+    },
+    {
+      title: 'Blogging for Social Change: Using Your Platform to Make a Difference',
+      body: 'Learn how to leverage your blog as a powerful tool for advocacy and social impact in todays interconnected world.'
+    },
+    {
+      title: 'The Science of Engaging Content: Strategies for Captivating Your Readers',
+      body: 'Delve into the psychology behind creating compelling content that resonates with your audience and keeps them coming back for more.'
+    },
+  ])
+
+}
+insertPostData()
+
 
 router.get('/about', (_req, res) => {
   res.render('about')
