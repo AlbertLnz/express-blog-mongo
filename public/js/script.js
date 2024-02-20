12eaaa1a1eb86e3f279c2352aa94a4d59cf66bb5
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-20 19:03:41 +0100

    Script to show/hidde searchBar

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,28 @@
-console.log('Hello from script.js')
+document.addEventListener('DOMContentLoaded', function() {
+
+  const searchBtn = document.querySelector('.searchBtn')
+  const searchBar = document.querySelector('.searchBar')
+  const searchBarInput = document.getElementById('searchInput')
+  const searchBarCloseBtn = document.getElementById('searchClose')
+
+  searchBtn.addEventListener('click', function() {
+
+    searchBar.style.visibility = 'visible'
+    searchBar.classList.add('open')
+
+    this.setAttribute('aria-expanded', 'true')
+
+    searchBarInput.focus()
+
+  })
+
+  searchBarCloseBtn.addEventListener('click', function() {
+
+    searchBar.style.visibility = 'hidden'
+    searchBar.classList.remove('open')
+
+    this.setAttribute('aria-expanded', 'false')
+
+  })
+
+})
