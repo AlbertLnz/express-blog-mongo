7851fe473a8984db26aad4b0b2b5317fafb7962e
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-20 17:42:46 +0100

    PostSchema created w/ mongoose

---
+++ server/models/Post.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+})
+
+const Post = mongoose.model('Post', PostSchema);
+
+export default Post
