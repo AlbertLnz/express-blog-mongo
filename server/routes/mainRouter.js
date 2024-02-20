37ce90366a6863196611a189089ffa5b1d476d85
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-20 17:51:10 +0100

    Pass data to '/' route and show them

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -1,15 +1,26 @@
 import express from 'express'
+import Post from '../models/Post.js'
 
 const router = express.Router()
 
-router.get('/', (_req, res) => {
+router.get('/', async (_req, res) => {
 
   const locals = {
     title: "NodeJS Blog",
     description: "Simple Blog created with NodeJS, ExpressJS & MongoDB" 
   }
 
-  res.render('index', locals)
+  try {
+    
+    const data = await Post.find()
+    res.render('index', { locals, data })
+
+  } catch (error) {
+    
+    console.log(error)
+
+  }
+
 })
 
 router.get('/about', (_req, res) => {
