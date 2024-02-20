6d340b7006efb3dc2ee4c305ecd920524c9b6355
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-20 17:21:32 +0100

    Set up connection of MongoDB Atlas successfully :)

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ import dotenv from 'dotenv'
 import express from 'express'
 import expressEjsLayouts from 'express-ejs-layouts'
 import mainRouter from './server/routes/mainRouter.js'
+import connectDB from './server/config/db.js'
 
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
+
+// MongoDB connection
+connectDB()
 
 // Middlewares
 app.use(express.static('public'))
